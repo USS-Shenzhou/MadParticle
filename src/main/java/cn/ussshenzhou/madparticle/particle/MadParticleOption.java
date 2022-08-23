6d33855fa1b787fac01d15d833840391baddd42d
USS-Shenzhou <969840795@qq.com>
2022-08-23 09:19:29 +0800

    turn MadParticleOption to record

--- src/main/java/cn/ussshenzhou/madparticle/particle/MadParticleOption.java
+++ src/main/java/cn/ussshenzhou/madparticle/particle/MadParticleOption.java
@@ -10,34 +10,17 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author USS_Shenzhou
  */
-public class MadParticleOption implements ParticleOptions {
-    private final int targetParticle;
-    private final MadParticle.SpriteFrom spriteFrom;
-    private final int amount;
-    private final double px, py, pz;
-    private final double xDiffuse, yDiffuse, zDiffuse;
-    private final double vx, vy, vz;
-    private final double vxDiffuse, vyDiffuse, vzDiffuse;
-    private final float friction;
-    private final float gravity;
-    private final boolean collision;
-    private final int bounceTime;
-    private final double horizontalRelativeCollisionDiffuse, verticalRelativeCollisionBounce;
-    private final float afterCollisionFriction;
-    private final float afterCollisionGravity;
-    private final boolean interactWithEntity;
-    private final double horizontalInteractFactor, verticalInteractFactor;
-    private final int lifeTime;
-    private final int renderType;
-    private final float r, g, b;
-    private final float beginAlpha, endAlpha;
-    private final MadParticle.ChangeMode alphaMode;
-    private final float beginScale, endScale;
-    private final MadParticle.ChangeMode scaleMode;
-    private final boolean alwaysRender;
-
-
-    public static final ParticleOptions.Deserializer<MadParticleOption> DESERIALIZER = new Deserializer<MadParticleOption>() {
+public record MadParticleOption(int targetParticle, MadParticle.SpriteFrom spriteFrom, int amount, double px, double py,
+                                double pz, double xDiffuse, double yDiffuse, double zDiffuse, double vx, double vy,
+                                double vz, double vxDiffuse, double vyDiffuse, double vzDiffuse, float friction,
+                                float gravity, boolean collision, int bounceTime,
+                                double horizontalRelativeCollisionDiffuse, double verticalRelativeCollisionBounce,
+                                float afterCollisionFriction, float afterCollisionGravity, boolean interactWithEntity,
+                                double horizontalInteractFactor, double verticalInteractFactor, int lifeTime,
+                                int renderType, float r, float g, float b, float beginAlpha, float endAlpha,
+                                MadParticle.ChangeMode alphaMode, float beginScale, float endScale,
+                                MadParticle.ChangeMode scaleMode, boolean alwaysRender) implements ParticleOptions {
+    public static final Deserializer<MadParticleOption> DESERIALIZER = new Deserializer<MadParticleOption>() {
         @Override
         public MadParticleOption fromCommand(ParticleType<MadParticleOption> pParticleType, StringReader pReader) throws CommandSyntaxException {
             return null;
@@ -75,61 +58,9 @@ public class MadParticleOption implements ParticleOptions {
                     interactWithEntity, horizontalInteractFactor, verticalInteractFactor,
                     lifeTime, renderType, r, g, b, beginAlpha, endAlpha, alphaMode, beginScale, endScale, scaleMode,
                     alwaysRender
-                    );
+            );
         }
     };
-
-    public MadParticleOption(int targetParticle, MadParticle.SpriteFrom spriteFrom, int amount,
-                             double px, double py, double pz, double xDiffuse, double yDiffuse, double zDiffuse,
-                             double vx, double vy, double vz, double vxDiffuse, double vyDiffuse, double vzDiffuse,
-                             float friction, float gravity, boolean collision, int bounceTime,
-                             double horizontalRelativeCollisionDiffuse, double verticalRelativeCollisionBounce, float afterCollisionFriction, float afterCollisionGravity,
-                             boolean interactWithEntity, double horizontalInteractFactor, double verticalInteractFactor,
-                             int lifeTime, int renderType,
-                             float r, float g, float b,
-                             float beginAlpha, float endAlpha, MadParticle.ChangeMode alphaMode,
-                             float beginScale, float endScale, MadParticle.ChangeMode scaleMode,
-                             boolean alwaysRender
-    ) {
-        this.targetParticle = targetParticle;
-        this.spriteFrom = spriteFrom;
-        this.amount = amount;
-        this.px = px;
-        this.py = py;
-        this.pz = pz;
-        this.xDiffuse = xDiffuse;
-        this.yDiffuse = yDiffuse;
-        this.zDiffuse = zDiffuse;
-        this.vx = vx;
-        this.vy = vy;
-        this.vz = vz;
-        this.vxDiffuse = vxDiffuse;
-        this.vyDiffuse = vyDiffuse;
-        this.vzDiffuse = vzDiffuse;
-        this.friction = friction;
-        this.gravity = gravity;
-        this.collision = collision;
-        this.bounceTime = bounceTime;
-        this.horizontalRelativeCollisionDiffuse = horizontalRelativeCollisionDiffuse;
-        this.verticalRelativeCollisionBounce = verticalRelativeCollisionBounce;
-        this.afterCollisionFriction = afterCollisionFriction;
-        this.afterCollisionGravity = afterCollisionGravity;
-        this.interactWithEntity = interactWithEntity;
-        this.horizontalInteractFactor = horizontalInteractFactor;
-        this.verticalInteractFactor = verticalInteractFactor;
-        this.lifeTime = lifeTime;
-        this.renderType = renderType;
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.beginAlpha = beginAlpha;
-        this.endAlpha = endAlpha;
-        this.alphaMode = alphaMode;
-        this.beginScale = beginScale;
-        this.endScale = endScale;
-        this.scaleMode = scaleMode;
-        this.alwaysRender = alwaysRender;
-    }
 
     @Override
     public void writeToNetwork(FriendlyByteBuf buf) {
@@ -181,157 +112,5 @@ public class MadParticleOption implements ParticleOptions {
     @Override
     public @NotNull String writeToString() {
         return "MadParticle";
-    }
-
-    public int getTargetParticle() {
-        return targetParticle;
-    }
-
-    public MadParticle.SpriteFrom getSpriteFrom() {
-        return spriteFrom;
-    }
-
-    public boolean isAlwaysRender() {
-        return alwaysRender;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public double getPx() {
-        return px;
-    }
-
-    public double getPy() {
-        return py;
-    }
-
-    public double getPz() {
-        return pz;
-    }
-
-    public double getxDiffuse() {
-        return xDiffuse;
-    }
-
-    public double getyDiffuse() {
-        return yDiffuse;
-    }
-
-    public double getzDiffuse() {
-        return zDiffuse;
-    }
-
-    public double getVx() {
-        return vx;
-    }
-
-    public double getVy() {
-        return vy;
-    }
-
-    public double getVz() {
-        return vz;
-    }
-
-    public double getVxDiffuse() {
-        return vxDiffuse;
-    }
-
-    public double getVyDiffuse() {
-        return vyDiffuse;
-    }
-
-    public double getVzDiffuse() {
-        return vzDiffuse;
-    }
-
-    public float getFriction() {
-        return friction;
-    }
-
-    public float getGravity() {
-        return gravity;
-    }
-
-    public boolean isCollision() {
-        return collision;
-    }
-
-    public int getBounceTime() {
-        return bounceTime;
-    }
-
-    public double getHorizontalRelativeCollisionDiffuse() {
-        return horizontalRelativeCollisionDiffuse;
-    }
-
-    public double getVerticalRelativeCollisionBounce() {
-        return verticalRelativeCollisionBounce;
-    }
-
-    public float getAfterCollisionFriction() {
-        return afterCollisionFriction;
-    }
-
-    public float getAfterCollisionGravity() {
-        return afterCollisionGravity;
-    }
-
-    public boolean isInteractWithEntity() {
-        return interactWithEntity;
-    }
-
-    public double getHorizontalInteractFactor() {
-        return horizontalInteractFactor;
-    }
-
-    public double getVerticalInteractFactor() {
-        return verticalInteractFactor;
-    }
-
-    public int getLifeTime() {
-        return lifeTime;
-    }
-
-    public int getRenderType() {
-        return renderType;
-    }
-
-    public float getR() {
-        return r;
-    }
-
-    public float getG() {
-        return g;
-    }
-
-    public float getB() {
-        return b;
-    }
-
-    public float getBeginAlpha() {
-        return beginAlpha;
-    }
-
-    public float getEndAlpha() {
-        return endAlpha;
-    }
-
-    public MadParticle.ChangeMode getAlphaMode() {
-        return alphaMode;
-    }
-
-    public float getBeginScale() {
-        return beginScale;
-    }
-
-    public float getEndScale() {
-        return endScale;
-    }
-
-    public MadParticle.ChangeMode getScaleMode() {
-        return scaleMode;
     }
 }

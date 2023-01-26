546ff70f20cb1400c2b0977cd5f8e8bddd9ece99
USS-Shenzhou <969840795@qq.com>
2023-01-26 15:03:34 +0800

    compatible with iris: downgrade render types.

--- src/main/java/cn/usshenzhou/madparticle/particle/ParticleRenderTypes.java
+++ src/main/java/cn/usshenzhou/madparticle/particle/ParticleRenderTypes.java
@@ -1,7 +1,11 @@
 package cn.usshenzhou.madparticle.particle;
 
 import cn.usshenzhou.madparticle.command.ToTranslatableString;
+import com.mojang.logging.LogUtils;
 import net.minecraft.client.particle.ParticleRenderType;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author USS_Shenzhou
@@ -13,12 +17,6 @@ public enum ParticleRenderTypes implements ToTranslatableString {
     TERRAIN_SHEET("gui.mp.de.helper.render_type.terrain"),
     PARTICLE_SHEET_OPAQUE("gui.mp.de.helper.render_type.opaque"),
     PARTICLE_SHEET_LIT("gui.mp.de.helper.render_type.lit"),
-
-    VANILLA_PARTICLE_SHEET_TRANSLUCENT("gui.mp.de.helper.render_type.vanilla_translucent"),
-    VANILLA_TERRAIN_SHEET("gui.mp.de.helper.render_type.vanilla_terrain"),
-    VANILLA_PARTICLE_SHEET_OPAQUE("gui.mp.de.helper.render_type.vanilla_opaque"),
-    VANILLA_PARTICLE_SHEET_LIT("gui.mp.de.helper.render_type.vanilla_lit"),
-
     CUSTOM("gui.mp.de.helper.render_type.custom"),
     NO_RENDER("gui.mp.de.helper.render_type.no_render");
 
@@ -32,37 +30,37 @@ public enum ParticleRenderTypes implements ToTranslatableString {
     public static ParticleRenderType getType(ParticleRenderTypes enumType) {
         switch (enumType) {
             case TERRAIN_SHEET -> {
-                return MadParticleRenderTypes.TERRAIN_SHEET;
+                return isIrisOn() ? ParticleRenderType.TERRAIN_SHEET : MadParticleRenderTypes.TERRAIN_SHEET;
             }
             case PARTICLE_SHEET_OPAQUE -> {
-                return MadParticleRenderTypes.PARTICLE_SHEET_OPAQUE;
+                return isIrisOn() ? ParticleRenderType.PARTICLE_SHEET_OPAQUE : MadParticleRenderTypes.PARTICLE_SHEET_OPAQUE;
             }
             case PARTICLE_SHEET_LIT -> {
-                return MadParticleRenderTypes.PARTICLE_SHEET_LIT;
+                return isIrisOn() ? ParticleRenderType.PARTICLE_SHEET_LIT : MadParticleRenderTypes.PARTICLE_SHEET_LIT;
             }
             case PARTICLE_SHEET_TRANSLUCENT -> {
-                return MadParticleRenderTypes.PARTICLE_SHEET_TRANSLUCENT;
+                return isIrisOn() ? ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT : MadParticleRenderTypes.PARTICLE_SHEET_TRANSLUCENT;
             }
-
-            case VANILLA_TERRAIN_SHEET -> {
-                return ParticleRenderType.TERRAIN_SHEET;
-            }
-            case VANILLA_PARTICLE_SHEET_OPAQUE -> {
-                return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
-            }
-            case VANILLA_PARTICLE_SHEET_LIT -> {
-                return ParticleRenderType.PARTICLE_SHEET_LIT;
-            }
-            case VANILLA_PARTICLE_SHEET_TRANSLUCENT -> {
-                return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
-            }
-
             case CUSTOM -> {
                 return ParticleRenderType.CUSTOM;
             }
             default -> {
                 return ParticleRenderType.NO_RENDER;
             }
+        }
+    }
+
+    private static boolean isIrisOn() {
+        try {
+            Class<?> irisApi = Class.forName("net.irisshaders.iris.api.v0.IrisApi");
+            Method getInstance = irisApi.getMethod("getInstance");
+            getInstance.setAccessible(true);
+            Method isShaderPackInUse = irisApi.getMethod("isShaderPackInUse");
+            isShaderPackInUse.setAccessible(true);
+            return (boolean) isShaderPackInUse.invoke(getInstance.invoke(null));
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
+                 InvocationTargetException ignored) {
+            return false;
         }
     }
 

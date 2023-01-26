98449ef7227a79342f38421e47f4b61cedf36b13
USS-Shenzhou <969840795@qq.com>
2023-01-26 13:44:27 +0800

    compatible with iris: downgrade render types.

--- src/main/java/cn/usshenzhou/madparticle/particle/MadParticle.java
+++ src/main/java/cn/usshenzhou/madparticle/particle/MadParticle.java
@@ -299,50 +299,53 @@ public class MadParticle extends TextureSheetParticle {
 
     @Override
     public void render(VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks) {
-        MadParticleBufferBuilder buffer = ((MadParticleRenderTypes) (particleRenderType)).bufferBuilder;
-        //copied from SingleQuadParticle.render for compatability with Rubidium
-        Vec3 vec3 = pRenderInfo.getPosition();
-        float f = (float) (Mth.lerp((double) pPartialTicks, this.xo, this.x) - vec3.x());
-        float f1 = (float) (Mth.lerp((double) pPartialTicks, this.yo, this.y) - vec3.y());
-        float f2 = (float) (Mth.lerp((double) pPartialTicks, this.zo, this.z) - vec3.z());
-        Quaternion quaternion;
-        if (this.roll == 0.0F) {
-            quaternion = pRenderInfo.rotation();
+        if (particleRenderType instanceof MadParticleRenderTypes){
+            MadParticleBufferBuilder buffer = ((MadParticleRenderTypes) (particleRenderType)).bufferBuilder;
+            //copied from SingleQuadParticle.render for compatability with Rubidium
+            Vec3 vec3 = pRenderInfo.getPosition();
+            float f = (float) (Mth.lerp((double) pPartialTicks, this.xo, this.x) - vec3.x());
+            float f1 = (float) (Mth.lerp((double) pPartialTicks, this.yo, this.y) - vec3.y());
+            float f2 = (float) (Mth.lerp((double) pPartialTicks, this.zo, this.z) - vec3.z());
+            Quaternion quaternion;
+            if (this.roll == 0.0F) {
+                quaternion = pRenderInfo.rotation();
+            } else {
+                quaternion = new Quaternion(pRenderInfo.rotation());
+                float f3 = Mth.lerp(pPartialTicks, this.oRoll, this.roll);
+                quaternion.mul(Vector3f.ZP.rotation(f3));
+            }
+
+            Vector3f vector3f1 = new Vector3f(-1.0F, -1.0F, 0.0F);
+            vector3f1.transform(quaternion);
+            Vector3f[] avector3f = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
+            float f4 = this.getQuadSize(pPartialTicks);
+
+            for (int i = 0; i < 4; ++i) {
+                Vector3f vector3f = avector3f[i];
+                vector3f.transform(quaternion);
+                vector3f.mul(f4);
+                vector3f.add(f, f1, f2);
+            }
+
+            float f7 = this.getU0();
+            float f8 = this.getU1();
+            float f5 = this.getV0();
+            float f6 = this.getV1();
+            int j = this.getLightColor(pPartialTicks);
+            buffer.vertex((double) avector3f[0].x(), (double) avector3f[0].y(), (double) avector3f[0].z()).uv(f8, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
+            buffer.bloomFactor(bloomFactor).endVertex();
+
+            buffer.vertex((double) avector3f[1].x(), (double) avector3f[1].y(), (double) avector3f[1].z()).uv(f8, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
+            buffer.bloomFactor(bloomFactor).endVertex();
+
+            buffer.vertex((double) avector3f[2].x(), (double) avector3f[2].y(), (double) avector3f[2].z()).uv(f7, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
+            buffer.bloomFactor(bloomFactor).endVertex();
+
+            buffer.vertex((double) avector3f[3].x(), (double) avector3f[3].y(), (double) avector3f[3].z()).uv(f7, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
+            buffer.bloomFactor(bloomFactor).endVertex();
         } else {
-            quaternion = new Quaternion(pRenderInfo.rotation());
-            float f3 = Mth.lerp(pPartialTicks, this.oRoll, this.roll);
-            quaternion.mul(Vector3f.ZP.rotation(f3));
+            super.render(pBuffer, pRenderInfo, pPartialTicks);
         }
-
-        Vector3f vector3f1 = new Vector3f(-1.0F, -1.0F, 0.0F);
-        vector3f1.transform(quaternion);
-        Vector3f[] avector3f = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
-        float f4 = this.getQuadSize(pPartialTicks);
-
-        for (int i = 0; i < 4; ++i) {
-            Vector3f vector3f = avector3f[i];
-            vector3f.transform(quaternion);
-            vector3f.mul(f4);
-            vector3f.add(f, f1, f2);
-        }
-
-        float f7 = this.getU0();
-        float f8 = this.getU1();
-        float f5 = this.getV0();
-        float f6 = this.getV1();
-        int j = this.getLightColor(pPartialTicks);
-        buffer.vertex((double) avector3f[0].x(), (double) avector3f[0].y(), (double) avector3f[0].z()).uv(f8, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
-        buffer.bloomFactor(bloomFactor).endVertex();
-
-        buffer.vertex((double) avector3f[1].x(), (double) avector3f[1].y(), (double) avector3f[1].z()).uv(f8, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
-        buffer.bloomFactor(bloomFactor).endVertex();
-
-        buffer.vertex((double) avector3f[2].x(), (double) avector3f[2].y(), (double) avector3f[2].z()).uv(f7, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
-        buffer.bloomFactor(bloomFactor).endVertex();
-
-        buffer.vertex((double) avector3f[3].x(), (double) avector3f[3].y(), (double) avector3f[3].z()).uv(f7, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j);
-        buffer.bloomFactor(bloomFactor).endVertex();
-
     }
 
     public static class Provider implements ParticleProvider<MadParticleOption> {
--- src/main/java/cn/usshenzhou/madparticle/particle/ParticleRenderTypes.java
+++ src/main/java/cn/usshenzhou/madparticle/particle/ParticleRenderTypes.java
@@ -13,6 +13,12 @@ public enum ParticleRenderTypes implements ToTranslatableString {
     TERRAIN_SHEET("gui.mp.de.helper.render_type.terrain"),
     PARTICLE_SHEET_OPAQUE("gui.mp.de.helper.render_type.opaque"),
     PARTICLE_SHEET_LIT("gui.mp.de.helper.render_type.lit"),
+
+    VANILLA_PARTICLE_SHEET_TRANSLUCENT("gui.mp.de.helper.render_type.vanilla_translucent"),
+    VANILLA_TERRAIN_SHEET("gui.mp.de.helper.render_type.vanilla_terrain"),
+    VANILLA_PARTICLE_SHEET_OPAQUE("gui.mp.de.helper.render_type.vanilla_opaque"),
+    VANILLA_PARTICLE_SHEET_LIT("gui.mp.de.helper.render_type.vanilla_lit"),
+
     CUSTOM("gui.mp.de.helper.render_type.custom"),
     NO_RENDER("gui.mp.de.helper.render_type.no_render");
 
@@ -37,6 +43,20 @@ public enum ParticleRenderTypes implements ToTranslatableString {
             case PARTICLE_SHEET_TRANSLUCENT -> {
                 return MadParticleRenderTypes.PARTICLE_SHEET_TRANSLUCENT;
             }
+
+            case VANILLA_TERRAIN_SHEET -> {
+                return ParticleRenderType.TERRAIN_SHEET;
+            }
+            case VANILLA_PARTICLE_SHEET_OPAQUE -> {
+                return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+            }
+            case VANILLA_PARTICLE_SHEET_LIT -> {
+                return ParticleRenderType.PARTICLE_SHEET_LIT;
+            }
+            case VANILLA_PARTICLE_SHEET_TRANSLUCENT -> {
+                return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+            }
+
             case CUSTOM -> {
                 return ParticleRenderType.CUSTOM;
             }
